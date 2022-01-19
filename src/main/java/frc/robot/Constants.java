b8287ee9b39f3703d1bd97b03d3ad72a7a6396fc
JohnPerg <71334884+JohnPerg@users.noreply.github.com>
2022-01-19 14:50:32 -0500

    add a default auto

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,11 +7,15 @@ package frc.robot;
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
@@ -21,9 +25,12 @@ public final class Constants {
     public static final int kRightMotor1Port = 2;
     public static final int kRightMotor2Port = 3;
 
+    public static int kShiftSolenoidPort = 0;
+    public static int kDriveSolenoidPort = 0;
+
     public static final double kTrackwidthMeters = 0.69;
-    public static final DifferentialDriveKinematics kDriveKinematics =
-        new DifferentialDriveKinematics(kTrackwidthMeters);
+    public static final DifferentialDriveKinematics kDriveKinematics = new DifferentialDriveKinematics(
+        kTrackwidthMeters);
 
     public static final int kEncoderCPR = 1024;
     public static final double kWheelDiameterMeters = 0.15;
@@ -32,9 +39,11 @@ public final class Constants {
         (kWheelDiameterMeters * Math.PI) / (double) kEncoderCPR;
 
     // These are example values only - DO NOT USE THESE FOR YOUR OWN ROBOT!
-    // These characterization values MUST be determined either experimentally or theoretically
+    // These characterization values MUST be determined either experimentally or
+    // theoretically
     // for *your* robot's drive.
-    // The Robot Characterization Toolsuite provides a convenient tool for obtaining these
+    // The Robot Characterization Toolsuite provides a convenient tool for obtaining
+    // these
     // values for your robot.
     public static final double ksVolts = 0.22;
     public static final double kvVoltSecondsPerMeter = 1.98;
@@ -42,8 +51,6 @@ public final class Constants {
 
     // Example value only - as above, this must be tuned for your drive!
     public static final double kPDriveVel = 8.5;
-    public static int kShiftSolenoidPort;
-    public static int kDriveSolenoidPort;
   }
 
   public static final class ControllerConstants {
@@ -61,7 +68,8 @@ public final class Constants {
     public static final double kMaxSpeedMetersPerSecond = 3;
     public static final double kMaxAccelerationMetersPerSecondSquared = 3;
 
-    // Reasonable baseline values for a RAMSETE follower in units of meters and seconds
+    // Reasonable baseline values for a RAMSETE follower in units of meters and
+    // seconds
     public static final double kRamseteB = 2;
     public static final double kRamseteZeta = 0.7;
   }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -33,98 +33,96 @@ import java.util.List;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-    // The robot's subsystems
-    private final DriveSubsystem m_robotDrive = new DriveSubsystem();
+        // The robot's subsystems
+        private final DriveSubsystem m_robotDrive = new DriveSubsystem();
 
-    public static final SendableChooser<Command> m_chooser = new SendableChooser<>();
+        public static final SendableChooser<Command> m_chooser = new SendableChooser<>();
 
-    /**
-     * The container for the robot. Contains subsystems, OI devices, and commands.
-     */
-    public RobotContainer() {
-        // Configure the button bindings
-        OI.configureButtonBindings(m_robotDrive);
+        /**
+         * The container for the robot. Contains subsystems, OI devices, and commands.
+         */
+        public RobotContainer() {
+                // Configure the button bindings
+                OI.configureButtonBindings(m_robotDrive);
 
-        // Configure default commands
-        // Set the default drive command to split-stick arcade drive
-        m_robotDrive.setDefaultCommand(
-                // A split-stick arcade command, with forward/backward controlled by the left
-                // hand, and turning controlled by the right.
-                new RunCommand(
-                        () -> m_robotDrive.arcadeDrive(
-                                -OI.getDriverLeftY(), OI.getDriverRightX()),
-                        m_robotDrive));
-        
-    m_chooser.addOption("s - curve",  Ramsete(makeSTrajectory()));
-    }
+                // Configure default commands
+                // Set the default drive command to split-stick arcade drive
+                m_robotDrive.setDefaultCommand(
+                                // A split-stick arcade command, with forward/backward controlled by the left
+                                // hand, and turning controlled by the right.
+                                new RunCommand(
+                                                () -> m_robotDrive.arcadeDrive(
+                                                                -OI.getDriverLeftY(), OI.getDriverRightX()),
+                                                m_robotDrive));
 
-    /**
-     * Use this to pass the autonomous command to the main {@link Robot} class.
-     *
-     * @return the command to run in autonomous
-     */
-    public Command getAutonomousCommand() {
-        return m_chooser.getSelected();
-    }
+                m_chooser.addOption("s - curve", Ramsete(makeSTrajectory()));
+        }
 
-    public Trajectory makeSTrajectory(){
-        
-        // Create a voltage constraint to ensure we don't accelerate too fast
-        var autoVoltageConstraint = new DifferentialDriveVoltageConstraint(
-                new SimpleMotorFeedforward(
-                        DriveConstants.ksVolts,
-                        DriveConstants.kvVoltSecondsPerMeter,
-                        DriveConstants.kaVoltSecondsSquaredPerMeter),
-                DriveConstants.kDriveKinematics,
-                10);
-        
-        // Create config for trajectory
-        TrajectoryConfig config = new TrajectoryConfig(
-                AutoConstants.kMaxSpeedMetersPerSecond,
-                AutoConstants.kMaxAccelerationMetersPerSecondSquared)
-                        // Add kinematics to ensure max speed is actually obeyed
-                        .setKinematics(DriveConstants.kDriveKinematics)
-                        // Apply the voltage constraint
-                        .addConstraint(autoVoltageConstraint);
+        /**
+         * Use this to pass the autonomous command to the main {@link Robot} class.
+         *
+         * @return the command to run in autonomous
+         */
+        public Command getAutonomousCommand() {
+                return m_chooser.getSelected();
+        }
 
-        // An example trajectory to follow. All units in meters.
-        Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
-                // Start at the origin facing the +X direction
-                new Pose2d(0, 0, new Rotation2d(0)),
-                // Pass through these two interior waypoints, making an 's' curve path
-                List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
-                // End 3 meters straight ahead of where we started, facing forward
-                new Pose2d(3, 0, new Rotation2d(0)),
-                // Pass config
-                config);
+        public Trajectory makeSTrajectory() {
 
-        return exampleTrajectory;
-    }
-    
-    
-    
-    public Command Ramsete(Trajectory exampleTrajectory) {
+                // Create a voltage constraint to ensure we don't accelerate too fast
+                var autoVoltageConstraint = new DifferentialDriveVoltageConstraint(
+                                new SimpleMotorFeedforward(
+                                                DriveConstants.ksVolts,
+                                                DriveConstants.kvVoltSecondsPerMeter,
+                                                DriveConstants.kaVoltSecondsSquaredPerMeter),
+                                DriveConstants.kDriveKinematics,
+                                10);
 
-        RamseteCommand ramseteCommand = new RamseteCommand(
-                exampleTrajectory,
-                m_robotDrive::getPose,
-                new RamseteController(AutoConstants.kRamseteB, AutoConstants.kRamseteZeta),
-                new SimpleMotorFeedforward(
-                        DriveConstants.ksVolts,
-                        DriveConstants.kvVoltSecondsPerMeter,
-                        DriveConstants.kaVoltSecondsSquaredPerMeter),
-                DriveConstants.kDriveKinematics,
-                m_robotDrive::getWheelSpeeds,
-                new PIDController(DriveConstants.kPDriveVel, 0, 0),
-                new PIDController(DriveConstants.kPDriveVel, 0, 0),
-                // RamseteCommand passes volts to the callback
-                m_robotDrive::tankDriveVolts,
-                m_robotDrive);
+                // Create config for trajectory
+                TrajectoryConfig config = new TrajectoryConfig(
+                                AutoConstants.kMaxSpeedMetersPerSecond,
+                                AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+                                                // Add kinematics to ensure max speed is actually obeyed
+                                                .setKinematics(DriveConstants.kDriveKinematics)
+                                                // Apply the voltage constraint
+                                                .addConstraint(autoVoltageConstraint);
 
-        // Reset odometry to the starting pose of the trajectory.
-        m_robotDrive.resetOdometry(exampleTrajectory.getInitialPose());
+                // An example trajectory to follow. All units in meters.
+                Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
+                                // Start at the origin facing the +X direction
+                                new Pose2d(0, 0, new Rotation2d(0)),
+                                // Pass through these two interior waypoints, making an 's' curve path
+                                List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
+                                // End 3 meters straight ahead of where we started, facing forward
+                                new Pose2d(3, 0, new Rotation2d(0)),
+                                // Pass config
+                                config);
 
-        // Run path following command, then stop at the end.
-        return ramseteCommand.andThen(() -> m_robotDrive.tankDriveVolts(0, 0));
-    }
+                return exampleTrajectory;
+        }
+
+        public Command Ramsete(Trajectory exampleTrajectory) {
+
+                RamseteCommand ramseteCommand = new RamseteCommand(
+                                exampleTrajectory,
+                                m_robotDrive::getPose,
+                                new RamseteController(AutoConstants.kRamseteB, AutoConstants.kRamseteZeta),
+                                new SimpleMotorFeedforward(
+                                                DriveConstants.ksVolts,
+                                                DriveConstants.kvVoltSecondsPerMeter,
+                                                DriveConstants.kaVoltSecondsSquaredPerMeter),
+                                DriveConstants.kDriveKinematics,
+                                m_robotDrive::getWheelSpeeds,
+                                new PIDController(DriveConstants.kPDriveVel, 0, 0),
+                                new PIDController(DriveConstants.kPDriveVel, 0, 0),
+                                // RamseteCommand passes volts to the callback
+                                m_robotDrive::tankDriveVolts,
+                                m_robotDrive);
+
+                // Reset odometry to the starting pose of the trajectory.
+                m_robotDrive.resetOdometry(exampleTrajectory.getInitialPose());
+
+                // Run path following command, then stop at the end.
+                return ramseteCommand.andThen(() -> m_robotDrive.tankDriveVolts(0, 0));
+        }
 }
